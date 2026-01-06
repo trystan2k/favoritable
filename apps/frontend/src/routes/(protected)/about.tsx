f391a0d9f8350981f7f6b198f1c46cd5e6ecc57e
Thiago Mendonca <trystan2k@gmail.com>
2026-01-06 14:17:07 +0100

    refactor: update css module imports to use lowercase filename

--- apps/frontend/src/routes/(protected)/about.tsx
+++ apps/frontend/src/routes/(protected)/about.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from '@tanstack/react-router';
 import { Button } from 'react-aria-components';
 import buttonSharedStyles from '../../shared-styles/Button.module.css';
 import { cx } from '../../utils/cx';
-import styles from './Layout.module.css';
+import styles from './layout.module.css';
 
 export const Route = createFileRoute('/(protected)/about')({
   component: RouteComponent,
